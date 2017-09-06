983752e4b72fbd660c6f9324a9815fa1fe81515f
Eagle <617701021@qq.com>
2017-09-06 20:21:32 +0800

    我写的一些简单的页面

---
+++ yemian/前端练习1/js/index.js
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PS设计后第一个网页</title>
+	<link rel="stylesheet" type="text/css" href="css/common.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<div class="top">
+		<div class="top_company" style="width: 1120px;">
+			<span style="color:#fff;line-height: 50px;">欢迎访问云财务公司</span>
+		</div>
+	</div>
+
+	<div class="header">
+		<div class="logo">
+			<img src="images/bg_03.png">
+			<span class="title">
+				<img src="images/bg_06.png">
+			</span>
+		</div>
+
+		<div class="channel">
+			<ul>
+				<li style="color:blue;">首&nbsp;&nbsp;&nbsp;&nbsp;页</li>
+				<li>业务介绍</li>
+				<li>关于我们</li>
+				<li>合作案例</li>
+				<li>联系我们</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="content">
+		<div class="banner">
+			<img src="images/bg_14.png">
+		</div>
+		<div style="clear:both;"></div>
+		<div class="zhong">
+			<div class="zhong_banner1">
+				<img src="images/bg_19.png">
+			</div>
+			<div class="zhong_banner2">
+				<img src="images/bg_22.png">
+			</div>
+			<div class="zhong_banner3">
+				<img src="images/bg_17.png">
+			</div>
+			<div class="zhong_banner4">
+				<img src="images/bg_25.png">
+			</div>
+		</div>
+	</div>	
+</body>
+</html>
