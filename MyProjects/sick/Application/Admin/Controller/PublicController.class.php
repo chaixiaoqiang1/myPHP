676f5af463c6c13bcb1d0e4efc57a02eaaf48d01
Eagle <617701021@qq.com>
2017-09-09 23:38:52 +0800

    代码优化
    代码一些优化处理

--- MyProjects/sick/Application/Admin/Controller/PublicController.class.php
+++ MyProjects/sick/Application/Admin/Controller/PublicController.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace Admin\Controller;
+
 use Think\Controller;
+
 class PublicController extends Controller
 {
     public function index()
@@ -8,21 +10,22 @@ class PublicController extends Controller
         $this->display();
     }
 
-    
-     public function login(){
-         if(IS_POST){
-           $username=I('post.username','trim');
-           $password=md5(I('post.password','trim'));
-           $User=M("ty_manager")->where("username='{$username}' and password='{$password}'")->find();
-           if(!empty($User)){
-                $_SESSION['admin_id']=$User['id'];
+    public function login()
+    {
+        if (IS_POST) {
+            $username = I('post.username', 'trim');
+            $password = md5(I('post.password', 'trim'));
+            $User = M("ty_manager")->where("username='{$username}' and password='{$password}'")->find();
+            if (!empty($User)) {
+                $_SESSION['admin_id'] = $User['id'];
                 $this->redirect('Index/index');
-           }
-         }
+            }
+        }
         $this->display();
     }
 
-    public function logout(){
+    public function logout()
+    {
         unset($_SESSION);
         session_destroy();
         $this->redirect('Public/login');
