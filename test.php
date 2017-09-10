25677c64cef2dfd21b376daa9695bc2a56212249
Eagle <617701021@qq.com>
2017-09-10 08:49:25 +0800

    无限级分类

--- test.php
+++ test.php
@@ -1,16 +1,6 @@
 <?php
 
-function generateTree($items){
-    $tree = array();
-    foreach($items as $item){
-        if(isset($items[$item['pid']])){
-            $items[$item['pid']]['son'][] = &$items[$item['id']];
-        }else{
-            $tree[] = &$items[$item['id']];
-        }
-    }
-    return $tree;
-}
+
 $items = array(
     1 => array('id' => 1, 'pid' => 0, 'name' => '安徽省'),
     2 => array('id' => 2, 'pid' => 0, 'name' => '浙江省'),
@@ -18,7 +8,24 @@ $items = array(
     4 => array('id' => 4, 'pid' => 3, 'name' => '长丰县'),
     5 => array('id' => 5, 'pid' => 1, 'name' => '安庆市'),
 );
-print_r(generateTree($items));die;
+
+// 无限级分类  常用的地方   导航   分类   地区
+$tree = array();
+foreach ($items as $k=>$v){
+    if($v['pid']){
+        $items[$v['pid']]['son'][] = &$items[$v['id']];
+        continue;
+    }
+    $tree[] = &$items[$v['id']];
+}
+
+
+print_r($tree);
+
+die;
+
+
+//print_r(generateTree($items));die;
 
 
 
