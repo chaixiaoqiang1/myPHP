ede228fc45c3048678d4f82ba1d79af13152f585
Eagle <617701021@qq.com>
2017-09-10 08:28:44 +0800

    修改了名字
    控制的很好

--- test.php
+++ test.php
@@ -1,4 +1,28 @@
 <?php
+
+function generateTree($items){
+    $tree = array();
+    foreach($items as $item){
+        if(isset($items[$item['pid']])){
+            $items[$item['pid']]['son'][] = &$items[$item['id']];
+        }else{
+            $tree[] = &$items[$item['id']];
+        }
+    }
+    return $tree;
+}
+$items = array(
+    1 => array('id' => 1, 'pid' => 0, 'name' => '安徽省'),
+    2 => array('id' => 2, 'pid' => 0, 'name' => '浙江省'),
+    3 => array('id' => 3, 'pid' => 1, 'name' => '合肥市'),
+    4 => array('id' => 4, 'pid' => 3, 'name' => '长丰县'),
+    5 => array('id' => 5, 'pid' => 1, 'name' => '安庆市'),
+);
+print_r(generateTree($items));die;
+
+
+
+
     // 字符串截取
     $str = "helloworld";
     echo substr($str,0);
